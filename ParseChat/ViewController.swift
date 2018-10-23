4b8d3ebb1964e70a062800b572f5a0bf6ae649f4
Abraham De Alba <abrahamdealba@abrahams-macbook-pro.local>
2018-10-22 17:01:15 -0700

    Initial Commit

---
+++ ParseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParseChat
+//
+//  Created by Abraham De Alba on 10/22/18.
+//  Copyright © 2018 Abraham De Alba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
